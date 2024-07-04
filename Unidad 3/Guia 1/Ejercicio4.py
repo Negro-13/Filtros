43a66f29237971087ae2afb121667ebcc70ecea0
Negro_13 <141974651+Negro-13@users.noreply.github.com>
2024-07-03 22:07:05 -0300

    Update Ejercicio4.py

--- Unidad 3/Guia 1/Ejercicio4.py
+++ Unidad 3/Guia 1/Ejercicio4.py
@@ -2,14 +2,25 @@ from PIL import Image
 
 print('Hola, pon la ruta de la imagen desea cortar')
 image = input()
-resolucion = image.size
+pokeimg = Image.open(image)
+resolucion = pokeimg.size
+width , height = pokeimg.size
 print(f'Esta es la resolucion de su imagen: {resolucion} ')
 print('Selecione las cordenadas en las q quiera cortar su imagen:')
 bi = int(input())
 bis = int(input())
 bd = int(input())
 bdi = int(input())
-pokeimg = Image.open(image)
-cutpoke = pokeimg.crop((bi, bis, bd, bdi))
-cutpoke.save('Guia 1\Recortes\pokecut.png')
+while True:
+    if bi > 0 and bi < width and bd > 1 and bd < width and bis > 0 and bis < height and bdi > 1 and bdi < height:
+        cutpoke = pokeimg.crop((bi, bis, bd, bdi))
+        break
+    else:
+        print('Selecione cordenadas validas')
+        bi = int(input())
+        bis = int(input())
+        bd = int(input())
+        bdi = int(input())
+        
+cutpoke.save('Recortes\imgcut.png')
 cutpoke.show()
